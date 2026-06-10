ff4c0231547e65061fc75fafa449c95ecd27db66
bluestack1732002-sys <bluestack1732002@gmail.com>
2026-06-10 09:12:49 +0530

    1st question's answer

---
+++ q1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+    int i1,i2;
+    printf("Enter 1st integer: ");
+    scanf("%d",&i1);
+    printf("Enter 1st integer: ");
+    scanf("%d",&i2);
+    if (i1 == i2)
+    {
+        printf("Both integers are equal.\n");
+    }
+    else{
+
+    printf("Not equal.\n");
+    }
+       
+
+
+    return 0;
+}
