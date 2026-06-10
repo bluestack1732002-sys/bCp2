f7d81904cf96f98fdcc6d26f957e64c0af08ace7
bluestack1732002-sys <bluestack1732002@gmail.com>
2026-06-10 10:23:40 +0530

    6th question's answer

---
+++ q6.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+    int n1, n2, n3;
+    printf("Enter 1st number: ");
+    scanf("%d",&n1);
+    printf("Enter 2nd number: ");
+    scanf("%d",&n2);
+    printf("Enter 3rd number: ");
+    scanf("%d",&n3);
+    if(n1 > n2 && n1>n3) {
+    printf("%d is the largest number.\n",n1);
+    }
+    else if (n2>n1 && n2>n3)
+    {
+        printf("%d is the largest number \n",n2);
+    }
+    else{
+    printf("%d is the largest number \n",n2);
+    }
+    return 0;
+}
